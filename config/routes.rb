5051aeaf6ddd59357a97c3b148d536d94ff42443
fanna <fanna993@gmail.com>
2015-05-28 17:07:47 +0200

    add login and register.html

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   resources :users do resources :users end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  get 'welcome/login'
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#login'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  #   get 'users/:users' => 'welcome#profile'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
